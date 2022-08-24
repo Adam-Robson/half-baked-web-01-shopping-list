14d4bf24f82c2226fad18ddace098d3e90acb182
Adam Robson <adamray312@gmail.com>
2022-08-23 22:12:18 -0700

    'build handle delete function'

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { checkAuth, signOutUser, addItem } from './fetch-utils.js';
+import { checkAuth, signOutUser, addItem, deleteItem, getAllItems } from './fetch-utils.js';
 
 import { renderItemContainer } from './render-utils.js';
 // checking if we have a user! (will redirect to auth if not):
@@ -9,6 +9,7 @@ checkAuth();
 const signOutLink = document.getElementById('sign-out-link');
 signOutLink.addEventListener('click', signOutUser);
 
+const shoppingListContainer = document.getElementById('shopping-list-container');
 const listItemInput = document.getElementById('list-item-input'); 
 const quantityInput = document.getElementById('quantity-input');   
 const addButton = document.getElementById('add-button');
@@ -30,3 +31,43 @@ addButton.addEventListener('click', async () => {
     listItemInput.value = '';
     quantityInput.value = '';
 });
+
+async function loadPage() {
+    const response = await getAllItems();
+    if (response.error) {
+        //eslint-disable-next-line no-console
+        console.log(response.error.message);
+    } else {
+        response.data;
+        displayItems();
+    }
+}
+loadPage();
+
+
+async function handleDelete(item) {
+    const message = `Delete this item?`;
+    if (!confirm(message)) return;
+
+    const response = await deleteItem(item.id);
+    if (!response.error) {
+        const items = await getAllItems();
+        const index = items.indexOf(item);
+        if (index !== -1) {
+            items.splice(index, 1);
+        }
+        getAllItems();
+        displayItems();
+    }
+} 
+
+async function displayItems() {
+    shoppingListContainer.innerHTML = '';
+
+    const items = await getAllItems();
+    for (let item of items) {
+        const renderedItem = renderItemContainer(item, handleDelete);
+        shoppingListContainer.append(renderedItem);
+    }
+ 
+}
--- render-utils.js
+++ render-utils.js
@@ -1,16 +1,26 @@
-export function renderItemContainer(item, quantity) {
+export function renderItemContainer(items, handleDelete) {
     const container = document.createElement('div');
     container.classList.add('list-item');
+   
+    for (let item of items) {
+        const listItem = document.createElement('p');
+        listItem.classList.add('list-item');
+        listItem.textContent = item.item;
 
-    const listItem = document.createElement('p');
-    listItem.classList.add('list-item');
-    listItem.textContent = item.item;
+        const quantityItem = document.createElement('p');
+        quantityItem.classList.add('quantity');
+        quantityItem.textContent = item.quantity;
 
-    const quantityItem = document.createElement('p');
-    quantityItem.classList.add('quantity');
-    quantityItem.textContent = item.quantity;
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-button');
+        deleteButton.textContent = 'x';
 
-    container.append(listItem, quantity);
+        deleteButton.addEventListener('click', () => {
+            handleDelete(item);
+        });
 
-    return container;
+        container.append(listItem, quantityItem, deleteButton);
+
+        return container;
+    }
 }
