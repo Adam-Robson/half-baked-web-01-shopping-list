4ba4530a01b3f5c368d677f7e1460bffdb1c2f29
Adam Robson <adamray312@gmail.com>
2022-08-23 18:50:23 -0700

    'clean up code'

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-// importing other stuff, utility functions for:
-// working with supabase:
 import { checkAuth, signOutUser, addItem } from './fetch-utils.js';
-// pure rendering (data --> DOM):
+
 import { renderItemContainer } from './render-utils.js';
-/*  "boiler plate" auth code */
 // checking if we have a user! (will redirect to auth if not):
 checkAuth();
 // can optionally return the user:
 // const user = checkAuth();
 
-// sign out link:
 const signOutLink = document.getElementById('sign-out-link');
 signOutLink.addEventListener('click', signOutUser);
-/* end "boiler plate auth code" */
 
-// grab needed DOM elements on page:
-    // section - inputs
-    // label - for - list item input
-    // id - list item input
-    // button - add button
-    // section - shopping list container
-    // div - list
 const listItemInput = document.getElementById('list-item-input'); 
 const quantityInput = document.getElementById('quantity-input');   
 const addButton = document.getElementById('add-button');
@@ -42,14 +30,3 @@ addButton.addEventListener('click', async () => {
     listItemInput.value = '';
     quantityInput.value = '';
 });
-
-
-
-
-
-
-// local state:
-
-// display functions:
-
-// events:
